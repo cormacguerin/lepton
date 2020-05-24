e335a1d5e51db87ab773133715560a316696bc5e
Cormac Guerin <cormacguerin@gmail.com>
2020-05-24 11:15:13 +0000

    forgot

--- api/postgres.js
+++ api/postgres.js
@@ -208,9 +208,9 @@ console.log("promises finished in " + totaltime + "ms");
     var query = "CREATE DATABASE \""
       + database
       + "\" WITH owner postgres"
-      + " ENCODING 'UTF8'"
-      + " LC_COLLATE = 'en_US.UTF-8'"
-      + " LC_CTYPE = 'en_US.UTF-8';"
+      + " ENCODING 'UTF8'";
+    //  + " LC_COLLATE = 'en_US.UTF-8'"
+    //  + " LC_CTYPE = 'en_US.UTF-8';"
 
     var vm = this;
     this.execute(query, null, function(e,r) {
